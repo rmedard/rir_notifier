6c024edee4a02a832155381489ba974fdf9ed1ed
rmedard <00Derfine@5>
2017-08-05 13:35:05 +0200

    Kubuntu

---
+++ src/Controller/NotifierController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medard
+ * Date: 03.08.17
+ * Time: 16:03
+ */
+
+namespace Drupal\rir_notifier\Controller;
+
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBuilder;
+use Drupal\webform\Entity\Webform;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class NotifierController extends ControllerBase {
+
+  protected $formBuilder;
+
+  public function __construct(FormBuilder $formBuilder) {
+    $this->formBuilder = $formBuilder;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('form_builder')
+    );
+  }
+
+  public function openSubscriptionModal(){
+    $response = new AjaxResponse();
+    $webform = Webform::load('notification_subscription');
+    $response->addCommand(new OpenModalDialogCommand($this->t('Free Email Alert'), $webform, ['width' => '800']));
+    return $response;
+  }
+}
