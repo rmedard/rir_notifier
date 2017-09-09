c9506bed40672f7a133af91ad592e222c3fd5455
rmedard <00Derfine@5>
2017-09-10 01:03:25 +0200

    Kubuntu

--- src/Service/DataAccessor.php
+++ src/Service/DataAccessor.php
@@ -47,8 +47,8 @@ class DataAccessor {
     }
 
     private function getQuery($location, $advert, $property){
-        $start_time = strtotime('-2 days 00:00:00');
-        $end_time = strtotime('-2 days 23:59:59');
+        $start_time = strtotime('-1 days 00:00:00');
+        $end_time = strtotime('-1 days 23:59:59');
         $query = Drupal::entityQuery('node')
           ->condition('type', 'advert')
           ->condition('status', 1)
