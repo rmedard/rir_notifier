4a1322a32298c98acd7bedbe36832962430c0106
rmedard <00Derfine@5>
2017-09-24 13:58:16 +0200

    Kubuntu

--- src/Plugin/QueueWorker/AlertsQueueWorker.php
+++ src/Plugin/QueueWorker/AlertsQueueWorker.php
@@ -16,8 +16,6 @@ use Mailchimp\Mailchimp;
 use Mailchimp\MailchimpAPIException;
 use Mailchimp\MailchimpLists;
 use function json_encode;
-use function md5;
-use function strtolower;
 
 
 /**
@@ -63,7 +61,6 @@ class AlertsQueueWorker extends QueueWorkerBase {
     public function processItem($data) {
 
         $createInterestPath = '/lists/{list_id}/interest-categories/{interest_category_id}/interests';
-        $addOrUpdateMemberPath = '/lists/{list_id}/members/{subscriber_hash}';
         $mailChimpAPIKey = $this->getMailchimpAPIKey();
         $mailchimp = new Mailchimp($mailChimpAPIKey);
         $mailchimpLists = new MailchimpLists($mailChimpAPIKey);
@@ -84,10 +81,6 @@ class AlertsQueueWorker extends QueueWorkerBase {
                     $response2 = NULL;
                     try {
 
-//                        $mailchimp->request('PUT', $addOrUpdateMemberPath,
-//                          array('list_id' => $this->getMailchimpListId(), 'subscriber_hash' => md5(strtolower($data->email))),
-//                          );
-
                         $response2 = $mailchimpLists->addOrUpdateMember($this->getMailchimpListId(), $data->email,
                           array(
                             'status' => MailchimpLists::MEMBER_STATUS_SUBSCRIBED,
@@ -97,7 +90,7 @@ class AlertsQueueWorker extends QueueWorkerBase {
                             'interests' => [$interestId => TRUE],
                             ), FALSE);
                         if (isset($response2) and !empty($response2->email_address)){
-                            Drupal::logger('rir_interface')->debug('Returned email: ' . $response2->email_address);
+                            Drupal::logger('rir_notifier')->debug('Returned email: ' . $response2->email_address);
                             $mailchimpLists->addSegmentMember($this->getMailchimpListId(), $segmentId, $response2->email_address);
                             Drupal::logger('rir_notifier')
                               ->notice('Member subscription updated: ' . $data->email . ' Response:' . json_encode($response2));
@@ -126,6 +119,7 @@ class AlertsQueueWorker extends QueueWorkerBase {
                 if ($segmentExists !== FALSE){
                     $segment = $segmentExists;
                 } else {
+                    Drupal::logger('rir_notifier')->debug('Create segment with email: ' . $data->email);
                     $segment = $mailchimpLists->addSegment($this->getMailchimpListId(), $data->reference, ['static_segment' => array($data->email)]);
                 }
                 $detailsRequestCategory = Node::create([
@@ -149,7 +143,7 @@ class AlertsQueueWorker extends QueueWorkerBase {
                         'interests' => [$interest->id => TRUE],
                       ), FALSE);
                     if (isset($response1) and !empty($response1->email_address)){
-                        Drupal::logger('rir_interface')->debug('Returned email: ' . $response1->email_address);
+                        Drupal::logger('rir_notifier')->debug('Returned email: ' . $response1->email_address);
                         $mailchimpLists->addSegmentMember($this->getMailchimpListId(), $segment->id, $response1->email_address);
                         Drupal::logger('rir_notifier')->notice('New member subscribed: ' . $data->email . ' Response:' . json_encode($response1));
                     } else {
