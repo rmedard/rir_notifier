2a7d9cb501d7f8eccd37115ef96fea5de4116929
rmedard <00Derfine@5>
2018-06-17 04:07:46 +0200

    Win10

--- src/Controller/NotifierController.php
+++ src/Controller/NotifierController.php
@@ -35,12 +35,12 @@ class NotifierController extends ControllerBase {
 
         $response = new AjaxResponse();
         $webform = Webform::load($values['webform_id'])->getSubmissionForm($values);
-        $options = array(
-            'width' => '80%',
-            'modal' => true,
-            'title' => 'Dore title'
-        );
-        $response->addCommand(new OpenModalDialogCommand($this->t('Free Email Alert'), $webform, $options));
+//        $options = array(
+//            'width' => '80%',
+//            'modal' => true,
+//            'title' => 'Dore title'
+//        );
+        $response->addCommand(new OpenModalDialogCommand($this->t('Free Email Alert'), $webform));
         return $response;
     }
 
