dd772621f705c2bba9625d57918058204f6554dc
Medard Rebero <medard.rebero@gmail.com>
2019-06-05 01:20:35 +0200

    Campaigns

--- src/Plugin/QueueWorker/CampaignQueueWorker.php
+++ src/Plugin/QueueWorker/CampaignQueueWorker.php
@@ -75,94 +75,21 @@ class CampaignQueueWorker extends QueueWorkerBase
             $submission = WebformSubmission::load($sid);
             $adverts = Node::loadMultiple($jobIds);
             if ($submission instanceof WebformSubmissionInterface) {
-                $to = $submission->getElementData('notif_email');
-                $params['subscriber_name'] = $submission->getElementData('notif_firstname');
+                $to = $submission->getElementData('notif_firstname') . '<' . $submission->getElementData('notif_email') . '>';
                 $params['message'] = Markup::create(getCampaignHtmlContent($sid, $submission->getElementData('notif_firstname'), $adverts));
+
+                $langcode = Drupal::currentUser()->getPreferredLangcode();
+                $send = TRUE;
+                $result = $mailManager->mail($module, $key, $to, $langcode, $params, $reply, $send);
+
+                if (intval($result['result']) !== 1) {
+                    $message = t('There was a problem sending campaign email to @email', ['@email' => $to]);
+                    Drupal::logger('rir_interface')->error($message);
+                } else {
+                    $message = t('An campaign email has been sent to @email.', ['@email' => $to]);
+                    Drupal::logger('rir_interface')->info($message);
+                }
             }
         }
-
-
-//        $storage = $this->entityTypeManager->getStorage('node');
-//        $query = $storage->getQuery()
-//            ->condition('type', 'advert')
-
-
-//        Drupal::logger('rir_notifier')->notice('Sending notifications started');
-//        $dataAccessor = Drupal::service('rir_notifier.data_accessor');
-//        $APIKey = $dataAccessor->getMailchimpAPIKey();
-//        $campaigns = new MailchimpCampaigns($APIKey);
-//        $lists = new MailchimpLists($APIKey);
-//        $mailchimp = new Mailchimp($APIKey);
-//        $ids = Drupal::entityQuery('node')
-//          ->condition('type', 'details_request_category')
-//          ->execute();
-//        $interests = Node::loadMultiple($ids);
-//        foreach ($interests as $interest) {
-//
-//            $reference = $interest->get('field_dr_reference')->value;
-//            if ($dataAccessor->countAdvertsByReference($reference) > 0) {
-//                $list_id = $interest->get('field_mailchimp_list_id')->value;
-//                $category_id = $interest->get('field_mailchimp_category_id')->value;
-//                $interest_ids = $interest->get('field_mailchimp_interest_id')->value;
-//                Drupal::logger('rir_notifier')
-//                  ->debug('list: ' . $list_id . ' category: ' . $category_id . ' interest: ' . $interest_ids);
-//                $members = $lists->getMembers($list_id,
-//                  [
-//                    'status' => MailchimpLists::MEMBER_STATUS_SUBSCRIBED,
-//                    'interest_category_id' => $category_id,
-//                    'interest_ids' => $interest_ids,
-//                    'interest_match' => 'all',
-//                  ]
-//                );
-//                $members_obj = (object) [
-//                  'list_id' => $list_id,
-//                  'segment_opts' => (object) ['saved_segment_id' => intval($interest->get('field_mailchimp_segment_id')->value)],
-//                ];
-//                $members_array = json_decode(json_encode($members, TRUE));
-//                if ($members_array->total_items > 0) {
-//                    $settings = [
-//                      'subject_line' => 'New properties matching your selection on Houseinrwanda.com',
-//                      'title' => 'Campaign - ' . $reference,
-//                      'from_name' => Drupal::config('system.site')->get('name'),
-//                      'reply_to' => Drupal::config('system.site')->get('mail'),
-//                      'inline_css' => TRUE,
-//                    ];
-//                    $campaign = NULL;
-//                    try {
-//                        $campaign = $campaigns->addCampaign(MailchimpCampaigns::CAMPAIGN_TYPE_REGULAR, $members_obj, (object) $settings);
-//                        if ($campaign) {
-//                            $campaign = json_decode(json_encode($campaign, TRUE));
-//                            Drupal::logger('rir_notifier')
-//                              ->debug('Created campaign: ' . $campaign->id);
-//                            try {
-//                                $obj = $campaigns->setCampaignContent($campaign->id, ['html' => getCampaignHtmlContent($reference)]);
-//                                if (!empty($obj)) {
-//                                    $campaigns->send($campaign->id, FALSE);
-//                                    //$mailchimp->processBatchOperations();
-//                                }
-//                                else {
-//                                    Drupal::logger('rir_notifier')
-//                                      ->debug('Empty campaign content: ' . $campaign->id);
-//                                }
-//                            } catch (MailchimpAPIException $exp1) {
-//                                Drupal::logger('rir_notifier')
-//                                  ->error('Sending Campaign: ' . $exp1->getMessage() . ' Full trace: ' . $exp1->getTraceAsString());
-//                            }
-//                        }
-//                    } catch (MailchimpAPIException $exp2) {
-//                        Drupal::logger('rir_notifier')
-//                          ->error('Creating Campaign: ' . $exp2->getMessage() . ' Full trace: ' . $exp2->getTraceAsString());
-//                    }
-//                }
-//                else {
-//                    Drupal::logger('rir_notifier')
-//                      ->debug('No subscribers found');
-//                }
-//            }
-//            else {
-//                Drupal::logger('rir_notifier')
-//                  ->debug('No adverts found for reference: ' . $reference);
-//            }
-//        }
     }
 }
