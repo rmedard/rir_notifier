d24a8423bbb30619cad32bd05e7c488a65128928
Medard Rebero <medard.rebero@gmail.com>
2019-06-04 20:31:31 +0200

    stop mailchimp

--- src/Service/DataAccessor.php
+++ src/Service/DataAccessor.php
@@ -14,6 +14,11 @@ use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\node\Entity\Node;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\taxonomy\TermStorageInterface;
+use Drupal\webform\Entity\Webform;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\WebformSubmissionStorage;
 use function strtotime;
 
 class DataAccessor
@@ -130,29 +135,66 @@ class DataAccessor
     public function getNotificationSubscribers() {
         try {
             $submissionsStorage = $this->entityTypeManager->getStorage('webform_submission');
-            $webform = Drupal\webform\Entity\Webform::load('notification_subscription');
-            if ($submissionsStorage instanceof Drupal\webform\WebformSubmissionStorage) {
-                foreach ($submissionsStorage->loadByEntities($webform, null, null) as $key => $submission) {
-                    if ($submission instanceof Drupal\webform\WebformSubmissionInterface) {
-                        $location = $submission->getElementData('notif_property_location');
-                        if (isset($location) and !empty($location)) {
+            $webform = Webform::load('notification_subscription');
+            if ($submissionsStorage instanceof WebformSubmissionStorage) {
 
-                            $possibilities = explode(' ', $location);
-                            foreach ($possibilities as $loc) {
-                                $loc = trim($loc);
-                                if (substr($loc, -1) === ',' or substr($loc, -1) === ';') $loc = substr($loc, 0, strlen($loc) - 1);
-                                if (strtolower($loc) === 'kigali') $loc = 'Kigali City';
-                                $term = $this->entityTypeManager->getStorage('taxonomy_term')
-                                    ->loadByProperties(['name' => ucwords($loc)]);
-                                if (isset($term) and !empty($term) and empty($submission->getElementData('property_location'))) {
-                                    $submission->setElementData('property_location', key($term));
-                                    $submission->save();
-                                    break;
+                $start_time = strtotime('-150 days 00:00:00');
+                $end_time = strtotime('-1 days 23:59:59');
+                $nodeStorage = $this->entityTypeManager->getStorage('node');
+
+                $subscribers = array();
+                foreach ($submissionsStorage->loadByEntities($webform, null, null) as $sid => $submission) {
+                    if ($submission instanceof WebformSubmissionInterface) {
+
+                        $advertType = $submission->getElementData('notif_advert_type');
+                        $propertyType = $submission->getElementData('notif_property_type');
+                        $location = $submission->getElementData('property_location');
+
+                        if (!isset($advertType) || strtolower($advertType) === 'all') {
+                            $advertType = null;
+                        }
+
+                        if (!isset($propertyType) || strtolower($propertyType) === 'all') {
+                            $propertyType = null;
+                        }
+
+                        if (!isset($location) || strtolower($location) === '0') {
+                            $location = null;
+                        }
+
+                        $query = $nodeStorage->getQuery()
+                            ->condition('type', 'advert')
+                            ->condition('status', Node::PUBLISHED)
+                            ->condition('created', array($start_time, $end_time), 'BETWEEN');
+
+                        if (isset($advertType)) {
+                            $query->condition('field_advert_type', $advertType);
+                        }
+
+                        if (isset($propertyType)) {
+                            $query->condition('field_advert_property_type', $propertyType);
+                        }
+
+                        if (isset($location)) {
+                            $term = Term::load(intval($location));
+                            $termStorage = $this->entityTypeManager->getStorage('taxonomy_term');
+                            if ($termStorage instanceof TermStorageInterface) {
+                                $locations = array();
+                                foreach ($termStorage->loadChildren($term->id()) as $locationTerm) {
+                                    array_push($locations, $locationTerm->id());
                                 }
+                                $query->condition('field_advert_district.target_id', $locations, 'IN');
                             }
+                        }
+
+                        $advertIds = $query->execute();
+
+                        if (isset($advertIds) and !empty($advertIds)) {
+                            $subscribers[$sid] = $advertIds;
                         }
                     }
                 }
+                return $subscribers;
             }
         } catch (InvalidPluginDefinitionException $e) {
             Drupal::logger('rir_notifier')
